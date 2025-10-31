03f3ca82ccaac547f12e1fb0a77f7158d60b5568
Tikhon Malinin <63498093+rearzty@users.noreply.github.com>
2025-10-31 17:40:27 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,30 @@
+import telebot
+from bot_logic import gen_pass
+from telebot.types import ReactionTypeEmoji
+import random
+    # Замени 'TOKEN' на токен твоего бота
+    # Этот токен ты получаешь от BotFather, чтобы бот мог работать
+bot = telebot.TeleBot("")
+
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.reply_to(message, "Привет! Я твой Telegram бот. Напиши что-нибудь!")
+
+@bot.message_handler(commands=['hello'])
+def send_hello(message):
+    bot.reply_to(message, "Привет! Как дела?")
+
+@bot.message_handler(commands=['bye'])
+def send_bye(message):
+    bot.reply_to(message, "Пока! Удачи!")
+
+@bot.message_handler(commands=['pass'])
+def send_bye(message):
+    bot.reply_to(message, "Привет, вот твой пароль: " + gen_pass(10))
+
+
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+    bot.reply_to(message, message.text)
+
+bot.polling()
